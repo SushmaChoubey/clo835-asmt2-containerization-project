cdf1591321496e3fe6633d1a48e1b76e17589a33
EC2 Default User <ec2-user@ip-172-31-39-196.ec2.internal>
2023-03-04 02:42:52 +0000

    added mysql pod manifest file

--- terraform_code/dev/ec2/install_httpd.sh
+++ terraform_code/dev/ec2/install_httpd.sh
@@ -2,6 +2,8 @@
 set -ex
 sudo yum update -y
 sudo yum install docker -y
+#For ping install
+sudo yum install iputils -y
 sudo systemctl start docker
 sudo usermod -a -G docker ec2-user
 curl -sLo kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
@@ -10,5 +12,15 @@ rm -f ./kind
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm -f ./kubectl
-kind create cluster --config kind.yaml​
+kind create cluster --config kind.yaml
+#For Images Download
+export ECR=983372048879.dkr.ecr.us-east-1.amazonaws.com/clo835-assigmt2-repo
+export DB_IMG=my_db_img
+export APP_IMG=my_app_img
+export DBPORT=3306
+export DBUSER=root
+export DATABASE=employees
+export DBPWD=db_pass123
+#Login into ECR repository
+aws ecr get-login-password --region us-east-1 |sudo docker login -u AWS ${ECR} --password-stdin
 EOF
