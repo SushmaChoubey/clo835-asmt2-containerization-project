8b632882b4d6cefe009ab6e0c3d0963186ddd148
EC2 Default User <ec2-user@ip-172-31-39-196.ec2.internal>
2023-03-08 19:57:29 +0000

    Deployment and Service Manifest

--- terraform_code/dev/ec2/install_httpd.sh
+++ terraform_code/dev/ec2/install_httpd.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-#For ping install
-sudo yum install iputils -y
-#For Images Download
+#ECR Repository
 export ECR=983372048879.dkr.ecr.us-east-1.amazonaws.com/clo835-assigmt2-repo
-export DB_IMG=my_db_img
-export APP_IMG=my_app_img
-export DBPORT=3306
-export DBUSER=root
-export DATABASE=employees
-export DBPWD=db_pass123
 #Login into ECR repository
 aws ecr get-login-password --region us-east-1 |sudo docker login -u AWS ${ECR} --password-stdin
 #Pull the Docker Image from AWS ECR in Kubernetes and creating Secret
@@ -17,15 +9,20 @@ kubectl get secret regcred --output=yaml
 #Namespace for running MYSQL POD MANIFEST
 kubectl create ns mysqlpod-namespace
 kubectl create -f mysql-manual.yaml -n mysqlpod-namespace
+kubectl create -f mysql-svc.yaml -n mysqlpod-namespace
 #Wait for MYSQLPOD to be ready
-kubectl wait --for=condition=Ready pod/mysql-pod -n mysqlpod-namespace
+kubectl wait --for=condition=Ready pod/mysql -n mysqlpod-namespace
 #Namespace for running PYTHON APP POD MANIFEST
 kubectl create ns mypythonpod-namespace
-#ConfigMap for Storing MYSQLDB IP Address
-kubectl create configmap my-configmap --from-literal=mydb-key=$(kubectl get pod mysql-pod --namespace=mysqlpod-namespace -o jsonpath='{.status.podIP}') -n mypythonpod-namespace
+#ConfigMap for Storing MYSQLDB Service Cluster IP
+kubectl create configmap mysqldb-configmap --from-literal=database_url=$(kubectl get svc mysqldb-service -n mysqlpod-namespace -o jsonpath='{.spec.clusterIP}') -n mypythonpod-namespace
 kubectl create -f python-app.yaml -n mypythonpod-namespace
 #Replicaset
 kubectl create -f mysql-rs.yaml -n mysqlpod-namespace
-# ConfigMap to get ClusterIP of DB service
-kubectl create configmap mysqldb-configmap --from-literal=database_url=$(kubectl get svc mysqldb-service -n mysqlpod-namespace -o jsonpath='{.spec.clusterIP}') -n mypythonpod-namespace
 kubectl create -f python-app-rs.yaml -n mypythonpod-namespace
+#Deployment
+kubectl create -f mysql-deployment.yaml -n mysqlpod-namespace
+kubectl create -f python-app-deployment.yaml -n mypythonpod-namespace
+#Creating NodePort Service
+kubectl create -f python-app-svc.yaml -n mypythonpod-namespace
+kubectl port-forward svc/python-app-service 8080:8080 -n mypythonpod-namespace
